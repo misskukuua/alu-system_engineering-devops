2d4228e743105cc41370fb8c5aeca24d118805eb
misskukuua <p.asare@alustudent.com>
2022-11-28 08:16:58 +0000

    ggg

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,2 @@
 #testing how well our web server setup featuring Nginx is doing under pressure
 #HTTP requests to a web server
-user { 'kuukua':
-  ensure     => present,
-  uid        => '1000',
-  gid        => '1000',
-  shell      => '/bin/bash',
-  home       => '/home/kuukua'
-}

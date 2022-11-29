0d2a56295ad94eb1dc36935395d8ec1f65a29a36
misskukuua <p.asare@alustudent.com>
2022-11-29 10:29:11 +0000

    all

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,5 @@
 #testing how well our web server setup featuring Nginx is doing under pressure
 #HTTP requests to a web server
-exec { 'more nginx server':
-    command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 2048\"/" /etc/default/nginx',
-    provider => shell,
-}
-
-exec { 'restart nginx':
-    command  => 'sudo service nginx restart',
-    provider => shell,
+exec { 'Fix NGINX Failed Requests':
+  command => "/bin/echo ULIMIT='-n 5000' | sudo tee /etc/default/nginx && sudo service nginx restart"
 }
